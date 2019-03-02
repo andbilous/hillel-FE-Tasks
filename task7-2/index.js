a441395e42bd33ad8a9fd0cd228ce7ff0d676cae
andbilous <andbilous@gmail.com>
2019-03-02 21:23:06 +0200

    l7-3

--- task7-2/index.js
+++ task7-2/index.js
@@ -5,12 +5,11 @@ function assignObjects1(obj1, obj2) {
 function assignObjects2(obj1, obj2, flag) {
   var obj1Clone = Object.assign({}, obj1);
   if (flag) {
-    return Object.assign(obj1, obj2, obj1Clone);
-  } else
-    return Object.assign(obj1, obj2);
+    return Object.assign(obj1, obj2,obj1Clone);
+  } return Object.assign(obj1, obj2);
 }
 
-console.log(assignObjects({
+console.log(assignObjects2({
   x: 110,
   y: 20,
   m: 40
@@ -19,7 +18,7 @@ console.log(assignObjects({
   s: 30
 }, false));
 
-console.log(assignObjects({
+console.log(assignObjects2({
   x: 110,
   y: 20,
   m: 40
