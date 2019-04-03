a6897bec1d5a1f804ccc43e2be02587d8de8a06b
andbilous <andbilous@gmail.com>
2019-04-03 22:51:47 +0300

    task25-26

--- task13-2/index.js
+++ task13-2/index.js
@@ -1,85 +1,75 @@
- const h=10;
- const step=30+'px';
- let hero=document.querySelector('#hero');
+const h = 10;
+const step = 30 + "px";
+let hero = document.querySelector("#hero");
 
- function moveElement(direction,value){
-    direction=margin+direction;
-   switch (direction){
-     case 'marginLeft':
+function moveElement(direction) {
+  switch (direction) {
+    case "left":
+      if (!hero.style.marginLeft) {
+        hero.style.marginLeft = h + "px";
+      } else {
+        let currentValue = parseInt(hero.style.marginLeft.replace("px", ""));
+        hero.style.marginLeft = h - currentValue + "px";
+      }
+      break;
+    case "right":
+      if (!hero.style.marginLeft) {
+        hero.style.marginLeft = h + "px";
+      } else {
+        let currentValue = parseInt(hero.style.marginLeft.replace("px", ""));
+        hero.style.marginLeft = h + currentValue + "px";
+      }
+      break;
+    case "up":
+      if (!hero.style.marginTop) {
+        hero.style.marginTop = h + "px";
+      } else {
+        let currentValue = parseInt(hero.style.marginTop.replace("px", ""));
+        hero.style.marginTop = currentValue - h + "px";
+      }
+      break;
+    case "down":
+      if (!hero.style.marginTop) {
+        hero.style.marginTop = h + "px";
+      } else {
+        let currentValue = parseInt(hero.style.marginTop.replace("px", ""));
+        hero.style.marginTop = currentValue + h + "px";
+      }
+      break;
+  }
+}
+function squat() {
+  let initialWidth = hero.offsetWidth;
+  let initialHeight = hero.offsetHeight;
+  heroWidth = hero.offsetWidth * 1.15;
+  heroHeight = hero.offsetHeight * 0.4;
+  hero.style.height = heroHeight + "px";
+  hero.style.width = heroWidth + "px";
+  window.onkeyup = e => {
+    hero.style.height = initialHeight + "px";
+    hero.style.width = initialWidth + "px";
+  };
+}
 
-   }
-
- }
- function squat(){
-  let initialWidth= hero.style.width.replace('px','');
-  let initialHeight=hero.style.height.replace('px','');
- 
-   window.onkeydown=()=>{
-     console.log('KeyDOwn');
-     console.log(hero.style);
-     console.log(hero.style.width);
-     console.log(hero.style.height);
-    hero.style.width=initialWidth*1.15+'px';
-    hero.style.height=initialHeight*0.4+'px';
-   }
-   window.onkeyup=()=>{
-    console.log(hero.style);
-    console.log(hero.style.width);
-    console.log(hero.style.height);
-    console.log('KeyUp');
-    hero.style.width=initialWidth+'px';
-    hero.style.height=initialHeight+'px';
-   }
- }
- window.onkeydown=(e)=>{
-  if(e.keyCode===32){
-    hero.style.animationName='jump';
+window.onkeydown = e => {
+  switch (e.keyCode) {
+    case 32:
+      hero.style.animationName = "jump";
+      break;
+    case 39:
+      moveElement("right");
+      break;
+    case 37:
+      moveElement("left");
+      break;
+    case 38:
+      moveElement("up");
+      break;
+    case 40:
+      moveElement("down");
+      break;
+    case 17:
+      squat();
+      break;
   }
-  if(e.keyCode===39){
-    if(!hero.style.marginLeft){
-      hero.style.marginLeft=h+'px';
-    }
-    else{
-      let currentValue=parseInt(hero.style.marginLeft.replace('px',''));
-      hero.style.marginLeft=h+currentValue+'px';
-    }
-    console.log('Right');
-  }
-  if(e.keyCode===37){
-    if(!hero.style.marginLeft){
-      hero.style.marginLeft=h+'px';
-    }
-    else{
-      let currentValue=parseInt(hero.style.marginLeft.replace('px',''));
-      hero.style.marginLeft=currentValue-h+'px';
-    }
-    console.log('Left');
-  }
-
-  if(e.keyCode===38){
-    if(!hero.style.marginTop){
-      hero.style.marginTop=h+'px';
-    }
-    else{
-      let currentValue=parseInt(hero.style.marginTop.replace('px',''));
-      hero.style.marginTop=currentValue-h+'px';
-    }
-    console.log('Up');
-  }
-
-  if(e.keyCode===40){
-    if(!hero.style.marginTop){
-      hero.style.marginTop=h+'px';
-    }
-    else{
-      let currentValue=parseInt(hero.style.marginTop.replace('px',''));
-      hero.style.marginTop=currentValue+h+'px';
-    }
-    console.log('Down');
-  }
-
-  if(e.ctrlKey){
-    squat();
-    console.log('CTRL');
-  }
- };
+};
