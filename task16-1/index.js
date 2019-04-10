854b196d2d6eaa6e60683a67ed223f4c0bb51069
andbilous <andbilous@gmail.com>
2019-04-10 10:57:01 +0300

    16-1

--- task16-1/index.js
+++ task16-1/index.js
@@ -1,75 +1,10 @@
-const h = 10;
-const step = 30 + "px";
-let hero = document.querySelector("#hero");
-
-function moveElement(direction) {
-  switch (direction) {
-    case "left":
-      if (!hero.style.marginLeft) {
-        hero.style.marginLeft = h + "px";
-      } else {
-        let currentValue = parseInt(hero.style.marginLeft.replace("px", ""));
-        hero.style.marginLeft = h - currentValue + "px";
-      }
-      break;
-    case "right":
-      if (!hero.style.marginLeft) {
-        hero.style.marginLeft = h + "px";
-      } else {
-        let currentValue = parseInt(hero.style.marginLeft.replace("px", ""));
-        hero.style.marginLeft = h + currentValue + "px";
-      }
-      break;
-    case "up":
-      if (!hero.style.marginTop) {
-        hero.style.marginTop = h + "px";
-      } else {
-        let currentValue = parseInt(hero.style.marginTop.replace("px", ""));
-        hero.style.marginTop = currentValue - h + "px";
-      }
-      break;
-    case "down":
-      if (!hero.style.marginTop) {
-        hero.style.marginTop = h + "px";
-      } else {
-        let currentValue = parseInt(hero.style.marginTop.replace("px", ""));
-        hero.style.marginTop = currentValue + h + "px";
-      }
-      break;
+let items = document.querySelectorAll('.cinema__item');
+let resPrice=0;
+console.log('Orders :');
+items.forEach((item)=>{
+  if(item.children[0].innerText &&item.children[1].innerText){
+    console.log(item.children[0].innerText);
+    resPrice+=parseInt(item.children[1].innerText);
   }
-}
-function squat() {
-  let initialWidth = hero.clientWidth;
-  let initialHeight = hero.clientHeight;
-  heroWidth = hero.offsetWidth * 1.15;
-  heroHeight = hero.offsetHeight * 0.4;
-  hero.style.height = heroHeight + "px";
-  hero.style.width = heroWidth + "px";
-  window.onkeyup = e => {
-    hero.style.height = initialHeight + "px";
-    hero.style.width = initialWidth + "px";
-  };
-}
-
-window.onkeydown = e => {
-  switch (e.keyCode) {
-    case 32:
-      hero.style.animationName = "jump";
-      break;
-    case 39:
-      moveElement("right");
-      break;
-    case 37:
-      moveElement("left");
-      break;
-    case 38:
-      moveElement("up");
-      break;
-    case 40:
-      moveElement("down");
-      break;
-    case 17:
-      squat();
-      break;
-  }
-};
+})
+console.log('Result price :'+resPrice);
