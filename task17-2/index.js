1012b1f6312c38534b3937d39513f9955c51f3e3
andbilous <andbilous@gmail.com>
2019-04-12 01:26:56 +0300

    task17-1-4

--- task17-2/index.js
+++ task17-2/index.js
@@ -1,9 +1,17 @@
-let matchPattern ='hello';
-let strings=["1234hello", "hellohd;lfmg", "45yu89egrhellowegojih", "dfhghello",",bnsdklb"];
+let response1=[];
+let response2=[];
+let xhr = new XMLHttpRequest();
+xhr.open('GET','https://jsonplaceholder.typicode.com/users',false);
+xhr.send();
+if(xhr.status!=200){
+  console.log(xhr.status+' : '+ xhr.statusText);
+}else
+response1=JSON.parse(xhr.responseText);
+xhr.open('GET','https://jsonplaceholder.typicode.com/posts/1/comments',false);
+xhr.send();
+if(xhr.status!=200){
+  console.log(xhr.status+' : '+ xhr.statusText);
+}else
+response2=JSON.parse(xhr.responseText);
+console.log([...response1,...response2]);
 
-let filteredStrings=strings.filter(string=> string.includes('hello'));
-
-let total = filteredStrings.reduce(
-  ( matchIndex, currentString ) =>
-  matchIndex+(currentString.match(matchPattern).index)+',',0);
-console.log(total);
