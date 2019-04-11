8a427f7665127c966621e3b6168288757814f7fa
andbilous <andbilous@gmail.com>
2019-04-12 02:28:04 +0300

    Update index.js

--- task17-2/index.js
+++ task17-2/index.js
@@ -1,17 +1,13 @@
-let response1=[];
-let response2=[];
 let xhr = new XMLHttpRequest();
-xhr.open('GET','https://jsonplaceholder.typicode.com/users',false);
-xhr.send();
-if(xhr.status!=200){
-  console.log(xhr.status+' : '+ xhr.statusText);
-}else
-response1=JSON.parse(xhr.responseText);
-xhr.open('GET','https://jsonplaceholder.typicode.com/posts/1/comments',false);
-xhr.send();
-if(xhr.status!=200){
-  console.log(xhr.status+' : '+ xhr.statusText);
-}else
-response2=JSON.parse(xhr.responseText);
-console.log([...response1,...response2]);
+function sendReq (url){
+  xhr.open('GET',url,false);
+  xhr.send();
+  if(xhr.status!=200){
+    console.log(xhr.status+' : '+ xhr.statusText);
+  }else
+return JSON.parse(xhr.responseText);
+}
+console.log([...sendReq('https://jsonplaceholder.typicode.com/users'),...sendReq('https://jsonplaceholder.typicode.com/posts/1/comments')]);
+
+
 
