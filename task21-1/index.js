e1202c4c8ca0e506ac3b6ee65c0d79acb2b1d198
andbilous <andbilous@gmail.com>
2019-05-08 01:18:50 +0300

    21/1

--- task21-1/index.js
+++ task21-1/index.js
@@ -1,19 +1,29 @@
-let obj1={
-    5:3,
-    'test':'4',
-    innerObj1:{
-        1:1,
-        2:2,
-        3:6,
-        'ss':'g'
+let obj1 = {
+    5: 3,
+    'test': '4',
+    'z': 'egweg',
+    innerObj1: {
+        1: 1,
+        2: 2,
+        3: 6,
+        'ss': 'g'
     },
-    calc:(obj)=>{
-        return Object.values(obj);
-    }
+}
 
+let obj2 = {
+    '2': '5',
+    calc: function () {
+        let sum = 0;
+        sum = Object.values(this).reduce((sum, elem) => {
+            if (typeof elem === 'string') {
+                return sum + 0;
+            }
+            if (typeof elem === 'object') {
+                return sum + obj2.calc.call(elem);
+            }
+            return sum + elem;
+        })
+        return sum;
+    }
 }
-let obj2={
-    '2':'5',
-    
-}
-console.log(obj1.calc());
+console.log(obj2.calc.call(obj1));
